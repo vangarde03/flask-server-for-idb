69a10e5644a975dfb5b69ba0292af53cc13f971b
vangarde03 <alexcobra7743@gmail.com>
2024-04-01 22:27:20 -0400

    testing some more

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from api.db_server import execute_query
+from db_server import execute_query
 
 app = Flask(__name__)
 # Allow requests from all origins to the /query endpoint
@@ -13,7 +13,8 @@ CORS(app, resources={r"/": {"origins": "*"}})
 
 
 @app.route('/')
-def query_endpoint():
+def home():
+    return request
     if request.method == 'GET':
         print("Received GET request to /query")
         return jsonify({"message": "GET request received, but this endpoint only accepts POST requests"}), 405
