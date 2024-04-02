304750eafd9391cd5815672e0deeb0f1d97e3caa
vangarde03 <alexcobra7743@gmail.com>
2024-04-01 21:53:53 -0400

    trying to get it to work

--- app.py
+++ app.py
@@ -1,28 +1,15 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from db_server import execute_query
+# app.py
+from flask import Flask
 
 app = Flask(__name__)
-# Allow requests from all origins to the /query endpoint
-CORS(app, resources={r"/query": {"origins": "*"}})
 
-# Your existing route and function definition
+@app.route('/', methods=['GET'])
+def home():
+    return "Homepage"
 
-
-@app.route('/query', methods=['GET', 'POST'])
-def query_endpoint():
-    if request.method == 'GET':
-        print("Received GET request to /query")
-        return jsonify({"message": "GET request received, but this endpoint only accepts POST requests"}), 405
-
-    data = request.json
-    print("Received request data:", data)
-    query = data.get('query')  # Get the query from the request
-    print("Query:", query)
-    result = execute_query(query)
-    print("Query result:", result)
-    return jsonify(result)
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/contact', methods=['GET'])
+def contact():
+    return "Contact page"
+    
+if __name__ == "__main__":
+    app.run() | Source: Genelify.com 
