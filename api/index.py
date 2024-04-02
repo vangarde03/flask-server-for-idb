d2198053055516d009be182d77bc118219814d05
vangarde03 <alexcobra7743@gmail.com>
2024-04-01 22:34:03 -0400

    moved execute query

--- api/index.py
+++ api/index.py
@@ -1,6 +1,49 @@
 from flask import Flask, jsonify, request
 # from flask_cors import CORS
-from db_server import execute_query
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_USERNAME = os.getenv("DATABASE_USERNAME")
+DB_PASSWORD = os.getenv("DATABASE_PASSWRD")
+DB_HOST = os.getenv("DATABASE_HOST")
+DB_NAME = "proj1part2"
+
+
+def execute_query(query):
+    try:
+        conn = psycopg2.connect(
+            dbname=DB_NAME, user=DB_USERNAME, password=DB_PASSWORD, host=DB_HOST
+        )
+        cur = conn.cursor()
+
+        if query.strip().upper().startswith('SELECT'):
+            cur.execute(query)
+            rows = cur.fetchall()
+            cur.close()
+            conn.close()
+            return rows
+        elif query.strip().upper().startswith('INSERT'):
+            cur.execute(query)
+            conn.commit()
+            cur.close()
+            conn.close()
+            return {'message': 'Query executed successfully'}
+        elif query.strip().upper().startswith('DELETE'):  # Handle DELETE queries
+            cur.execute(query)
+            conn.commit()
+            cur.close()
+            conn.close()
+            return {'message': 'DELETE query executed successfully'}
+        else:
+            return {'error': 'Unsupported query type'}
+
+    except Exception as e:
+        print("Error executing query:", e)
+        return {'error': str(e)}
+
 
 app = Flask(__name__)
 # Allow requests from all origins to the /query endpoint
