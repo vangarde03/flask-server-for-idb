011020790017b03f9658f41b929dabfd9fb401db
vangarde03 <alexcobra7743@gmail.com>
2024-04-01 22:17:06 -0400

    might work

--- api/index.py
+++ api/index.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from api.db_server import execute_query
 
 app = Flask(__name__)
+# Allow requests from all origins to the /query endpoint
+CORS(app, resources={r"/query": {"origins": "*"}})
+
+# Your existing route and function definition
 
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def query_endpoint():
+    if request.method == 'GET':
+        print("Received GET request to /query")
+        return jsonify({"message": "GET request received, but this endpoint only accepts POST requests"}), 405
+
+    data = request.json
+    print("Received request data:", data)
+    query = data.get('query')  # Get the query from the request
+    print("Query:", query)
+    result = execute_query(query)
+    print("Query result:", result)
+    return jsonify(result)
 
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == '__main__':
+    app.run()
